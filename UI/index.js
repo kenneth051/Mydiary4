a3e6b1c5058a71161d9b76fe44213b490335d005
kenneth051 <kjdumba@gmail.com>
2018-08-11 13:29:43 -0700

    Adding UI pages

---
+++ UI/index.js
@@ -0,0 +1,30 @@
+function login(){
+	window.location="login.html";
+}
+function register(){
+	window.location="index.html";
+}
+function home(){
+	document.location="home.html";
+}
+function profile(){
+	window.location="profile.html";
+}
+function editprofile(){
+	window.location="EditProfile.html";
+}
+function allentries(){
+	window.location="AllEntries.html";
+}
+function single(){
+	window.location="SingleEntry.html";
+}
+function logout(){
+	window.location="login.html";
+}
+function update(){
+	window.location="UpdateEntry.html";
+}
+function reminders(){
+	window.location="Reminders.html";
+}
