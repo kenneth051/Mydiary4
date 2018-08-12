b7fd4903bc95accdeeadb2cee80293acb4195bbc
kenneth051 <kjdumba@gmail.com>
2018-08-12 14:30:48 -0700

    Adding Javascript and modifying code

---
+++ UI/js/signup.js
@@ -0,0 +1,37 @@
+document.getElementById('signupform').addEventListener('submit', func)
+function func(e){
+    e.preventDefault();
+    var firstname1 = document.getElementById('firstname').value;
+    var lastname1 = document.getElementById('lastname').value;
+    var username1 = document.getElementById('username').value;
+    var email1 = document.getElementById('email').value;
+    var password1 = document.getElementById('password').value;
+    var confirmpassword1 = document.getElementById('confirmpassword').value;
+    var gender1 = document.getElementById('gender').value;
+    if(password1 === confirmpassword1){
+            const send = {firstname:firstname1, lastname:lastname1,username:username1,email:email1, password:password1, gender:gender1};
+            fetch('https://cryptic-basin-28780.herokuapp.com/API/v1/auth/user/signup', {
+                method: 'POST',
+                cache: 'no-cache',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(send)
+        }).then(function(response) {
+            return response.json();
+        }).then(function(data){
+            if (data.status_code != 201){
+                document.getElementById('msg').style.display = "block";
+                document.getElementById("msg").innerHTML=data.message
+            }if (data.status_code == 201){
+                document.getElementById("msg").innerHTML = "#redirect to login page here "
+            }
+
+        })
+    }else{
+        document.getElementById('msg').style.display = "block";
+        document.getElementById("msg").innerHTML = "ERROR! Passwords do not MATCH..."
+    }
+
+}
